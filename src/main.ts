a63ffb3ec9a615f0288b12fddfa77e19cbba1fe5
Rubicker <newsl00001@gmail.com>
2018-12-18 11:14:31 +0800

    :card_file_box: Initial database config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
 }
 bootstrap();
